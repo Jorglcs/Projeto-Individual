3047f1a1a5486cd9ad652872b33b4673ebd849a8
Jorglcs <jorge.siqueira@sptech.school>
2025-10-29 00:05:03 -0300

    Adicionando funcao de aleatoridade de perks para as portas

--- public/script.js
+++ public/script.js
@@ -5,17 +5,12 @@ var vidaMaximaInimigo = 100;
 var vidaInimigo = 100;
 var danoInimigo = 12;
 
-
-
-
-
-
-
 function atacar() {
   vidaInimigo -= danoAtaque;
   p_vidaInimigo.innerHTML = vidaInimigo;
   if (vidaInimigo <= 0) {
     selecionarPorta();
+    randomizarPerkPortas();
   }
   ataqueInimigo();
 }
@@ -40,11 +35,11 @@ function ataqueInimigo() {
   }
   p_vidaPlayer.innerHTML = `${vidaAtual}/${vidaMaxima}`;
 }
-var numPortas = 0
+var numPortas = 0;
 function selecionarPorta() {
   tudo.style.display = "none";
   div_portas.style.display = "flex";
-   numPortas = Math.floor(Math.random() * 3 + 1);
+  numPortas = Math.floor(Math.random() * 3 + 1);
   if (numPortas == 1) {
     button_porta1.style.display = "flex";
     button_porta2.style.display = "none";
@@ -62,18 +57,30 @@ function selecionarPorta() {
   }
 }
 
-function randomizarPortas(){
-  var cont =0
-  while (cont<numPortas){
-    cont++
-    var numEfeito = Math.floor(Math.random() * 4 + 1)
-    if (numEfeito ==1){
-      button_porta1.onclick = aresBoons;
-      img_porta
-    }
+function randomizarPerkPortas() {
+  var listaPerks = [
+    ["assets/images/centaurhearth.png", "Coração de centauro"],
+    ["assets/images/Ares_symbol.png", "Benção de Ares", aresBoons],
+    ["assets/images/Zeus_symbol.png", "Benção de Zeus"],
+    ["assets/images/Poseidon_symbol.png", "Benção de Poseidon"],
+  ];
+
+  // a lista dos perks vai ser [img,nome,descricao]
+
+  var cont = 0;
+  while (cont < numPortas) {
+    var tamanholistaPerks = listaPerks.length;
+    cont++;
+    var img_portaid = document.getElementById(`img_porta${cont}`);
+    var p_portaid = document.getElementById(`p_porta${cont}`);
+    var button_portaid = document.getElementById(`button_porta${cont}`);
+
+    var numListaAleatorio = Math.floor(Math.random() * tamanholistaPerks); //gerar numero de 0 a tamanho da lista -1 pq começa do 0
+    img_portaid.src = listaPerks[numListaAleatorio][0];
+    p_portaid.textContent = listaPerks[numListaAleatorio][1];
+    button_portaid.onclick = listaPerks[numListaAleatorio][2];
+    listaPerks.splice(numListaAleatorio, 1);
   }
-
-
 }
 
 function selecionarBoon() {
@@ -83,6 +90,7 @@ function selecionarBoon() {
 }
 
 function aresBoons() {
+  selecionarBoon();
   div_portas.style.display = "none";
   tudo.style.display = "none";
   div_tudoBoon.style.display = "flex";
